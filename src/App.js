8109f94b04a593ec794bf946c82510d8fea63ed2
Federico-I <threxos.48@gmail.com>
2024-06-25 22:43:07 +0100

    settingUp-useReducer-reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useReducer } from 'react';
 import Header from "./Header";
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+
+  // "loading", "error", "ready", "active", "finished"
+  sate: "loading",
+};
+
+function reducer(state, action) {
+
+  switch(action.type) {
+    case "dataReceived": 
+      return { 
+        ...state, 
+        question: action.payload,
+        status: "ready"
+      };
+    case "dataFailed":
+      return{
+        ...state,
+        status: "error",
+      };
+
+    default: 
+      throw new Error ("Action unknown");
+  };
+};
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect( function() {
+    fetch("https://localhost:3000/questions").then((res) => res.json()).then((data) => dispatch({ type: "dataReceived", payload: data})).catch((err) => dispatch("dataFailed"));
+  }, []);
+
   return (
     <div className="App">
       <Header />
