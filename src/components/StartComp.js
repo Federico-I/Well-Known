7812585b6f8d6bede88ae9973c980fbe559be32a
Federico-I <threxos.48@gmail.com>
2024-07-09 12:10:57 +0100

    avtionButtonBug-fixed optionComp-bug

--- src/components/StartComp.js
+++ src/components/StartComp.js
@@ -3,9 +3,9 @@ import React from 'react';
 function StartComp({ totalQuest, dispatch }) {
   return (
     <div className='start'>
-        <h2>Welcome! are you ready to test yourself about React?</h2>
+        <h2>Are you ready to test yourself about React?</h2>
         <h3> Total of {totalQuest} questions will challenge you, so if you're ready... lights... cameras... and...</h3>
-        <button className='btn btn-ui' onChange={() => dispatch({ type: "start"})}>
+        <button className='btn btn-ui' onClick={() => dispatch({ type: "start"})}>
           Action!
         </button>
     </div>
